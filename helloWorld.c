d3f6c4bdf17489359e76f5f46aac4bcd3e779374
Ayush Kumar Singh <112181404+AyushKr2003@users.noreply.github.com>
2022-09-14 16:53:25 +0530

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+
+// This is a single line comment. You can write comment in single line only
+int main() {
+    /*
+     This is a miltiple line comments
+     You can write comments in multiple line
+     like this comment is written in 3 line
+     */
+//    int num = 25;//this is integer data type
+//
+//    char star = '*';//this is a character data type
+//
+//    float pi = 3.14;// this is a float data type
+//
+//    printf("Learning C");
+//    printf("Learning C");// if we don't use '\n' then there is no space or line between two print function
+//
+//    printf("\nHello World\n");
+//    printf("Good Morning\n");// Funtion '/n' means gon to next line
+//
+//    printf("print num is %d\n",num);// symbol '%d' use for integer data type
+//
+//    printf("print star %c\n",star);// symbol '%c' use for character data type
+//
+//    printf("print pi is %f\n",pi);// symbol '%f' use for float data type
+//
+//    //Input function
+//    int age;
+//    printf("Enter your age:");
+//    scanf("%d",&age);
+//    printf("Your age is %d",age);
+//
+//    int a = 2;
+//    int b = 4;
+//    printf("%d", b / a && 2 == 2);//'&&' is use for 'and' statement
+//
+//    printf("%d",b / a || 2 == 2);//'||' is use for 'or' statement
+
+//    //increment operator
+//    //++i, i++
+//
+//    int i = 1;
+//    printf("%d", i++);  //use then increase
+//    printf("%d",i);
+//
+//    int i = 1;
+//    printf("%d", ++i);  //increase then use
+//    printf("%d",i);
+//
+//
+//
+//
+//
+//    //while loop
+//    int num;
+//    printf("Enter the number :");
+//    scanf("%d",&num);
+//
+//    int i = 0;
+//    while (i<=num) {
+//        printf("%d \n", i);
+//        i++;
+//    }
+//
+//
+//    //For loop
+//    for (int i= 1; i<=num; i++) {
+//        printf("%d \n", i);
+//    }
+
+
+
+
+    return 0;
+
+}
