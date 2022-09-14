99b95fa67cc5a285f212736cc9f46cc611aa5c0e
Ayush Kumar Singh <112181404+AyushKr2003@users.noreply.github.com>
2022-09-14 17:15:57 +0530

    Add files via upload

---
+++ fibonacci_Function.c
@@ -0,0 +1,54 @@
+//#include <stdio.h>
+//int main() {
+//
+//    int i, n;
+//
+//    // initialize first and second terms
+//    int t1 = 0, t2 = 1;
+//
+//    // initialize the next term (3rd term)
+//    int nextTerm = t1 + t2;
+//
+//    // get no. of terms from user
+//    printf("Enter the number of terms: ");
+//    scanf("%d", &n);
+//
+//    // print the first two terms t1 and t2
+//    printf("Fibonacci Series: %d, %d, ", t1, t2);
+//
+//    // print 3rd to nth terms
+//    for (i = 3; i <= n; ++i) {
+//        printf("%d, ", nextTerm);
+//        t1 = t2;
+//        t2 = nextTerm;
+//        nextTerm = t1 + t2;
+//    }
+//
+//    return 0;
+//}
+
+
+//method 2
+
+//#include <stdio.h>
+//
+//int fib(int n);
+//
+//int main() {
+//    printf("%d",fib(3));
+//
+//    return 0;
+//}
+//
+//int fib(int n){
+//    if (n==0){
+//        return 0;
+//    }
+//    if (n==1){
+//        return 1;
+//    }
+//    int fibNm1 = fib(n-1);
+//    int fibNm2 = fib(n-2);
+//    int fibN = fibNm1 + fibNm2;
+//    return fibN;
+//}
