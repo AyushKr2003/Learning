b1a56888a5adf74f44b323081d852fac78880869
Ayush Kumar Singh <112181404+AyushKr2003@users.noreply.github.com>
2022-09-14 17:27:39 +0530

    Add files via upload

---
+++ Problem.c
@@ -0,0 +1,140 @@
+#include<stdio.h>
+
+int main() {
+
+       //Problem 1
+
+//    int a, b;
+//    printf("Enter two number for addition\n");
+//    printf("Enter a:");
+//    scanf("%d", &a);
+//
+//    printf("Enter b:");
+//    scanf("%d", &b);
+//
+//    int sum = a+b;
+//    printf("Sum : %d", sum);
+//
+//
+//
+//    // Problem 2
+//
+//    float a;
+//
+//    printf("Enter redius of the circle:");
+//    scanf("%f", &a);
+//
+//    printf("Area is : %f", a * a * 3.14);
+//
+//
+//
+//    //Problem3
+//
+//    int a;
+//    printf("Enter day number:");
+//    scanf("%d", &a);
+//    switch (a) {
+//        case 1:
+//            printf("Monday");
+//            break;
+//
+//        case 2:
+//            printf("Tuesday");
+//            break;
+//
+//        case 3:
+//            printf("Wednesday");
+//            break;
+//
+//        case 4:
+//            printf("Thursday");
+//            break;
+//
+//        case 5:
+//            printf("Friday");
+//            break;
+//
+//        case 6:
+//            printf("Saturday");
+//            break;
+//
+//        case 7:
+//            printf("Sunday");
+//            break;
+//
+//        default:
+//            printf("Try again");
+//    }
+//
+//
+//    //Problem 4
+//    int isMonday = 1;
+//    int itsRaining = 0;
+//    printf("%d", isMonday || itsRaining); //print 1(true) if only any one is true
+//
+//
+//
+//    //Problem 5
+//
+//    method 1
+//     int marks;
+//     printf("Enter the marks(in 0-100): ");
+//     scanf("%d",&marks);
+//
+//     if (marks > 30 && marks <=100) {
+//         printf("Pass\n");
+//
+//     }
+//     else if (marks <= 30 && marks >= 0) {
+//         printf("Fail\n");
+//     }
+//
+//     else {
+//         printf("Please enter your correct marks again\n");
+//     }
+//
+//
+//
+//    //method 2
+//    (marks > 30) ? printf("Pass") : printf("Fail");
+//
+//
+//
+//
+//
+//
+//    //Problem 6
+//
+//     char ch;
+//     printf("Enter a character :");
+//     scanf("%c", &ch);
+//
+//     if ( ch >= 'a' && ch<='z') {
+//         printf("lower case\n");
+//     }
+//     else if ( ch >= 'A' && ch <='Z') {
+//         printf("UPPER CASE\n");
+//     }
+//
+//     else {
+//         printf("Invalid input\n");
+//     }
+//
+//
+//
+//
+//    //Problem
+//    int table;
+//    printf("Enter the number:");
+//    scanf("%d",&table);
+//
+//    for (int i=1; i<=10; i++){
+//        int x = i*table;
+//        printf("%d \n",x);
+//    }
+
+
+
+    return 0;
+
+}
